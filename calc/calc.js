b194fd1f75f64cb89e56f78f176a5bc03d227d58
pedro <joao.pedro73210@gmail.com>
2022-04-25 20:31:07 -0300

    teste 1 dos botões com as funções criadas

--- calc/calc.js
+++ calc/calc.js
@@ -1,6 +1,6 @@
 var aux = 0
 var valores = []
-var func = ""
+var func = 0
 var res = 0
 
 //criando a função para inserir o valor dos botões no visor
@@ -18,19 +18,65 @@ function pegaValor(){
     let cont = document.getElementById("resultado").value
     console.log(cont)
     valores.push(cont)
+    console.log(valores[valores.length -  1])
+    //console.log(valores[valores.length-  2])
 }
 
 //criando função de identificar a operação a ser executada
 
 function operations(b){
     func = b
+    
     console.log(func)
+    
     aux++
-    console.log(aux)
+        
     pegaValor()
+    
     let a = document.querySelector('input[name="res"]')
-    a.value =""
-    if(valores.lenght = 2){
+    a.value = ""
+}
+//criando funções auxiliares
+
+function soma (a,b){
+    return a+b
+}
+
+function sub (a,b){
+    return a-b
+}
+
+function mult (a,b){
+    return a*b
+}
+
+function div (a,b){
+    return a/b
+}
+
+//criando função para alertar o valor do campo visor
+
+function mostraValor () {
+
+    pegaValor()
+
+    console.log("valores no array")
+
+    console.log("-----")
+
+    console.log(valores.length)
+
+    console.log("------")
+
+    for (let i = 0; i < valores.length; i++){
+        console.log(valores[i])
+    }
+
+    console.log("-------")
+
+    if (valores.length = 1){
+        
+    } else if(valores.lenght = 2){
         switch (b){
             case '+':
                 valores.push(valores[0] + valores[1])
@@ -53,24 +99,18 @@ function operations(b){
                 valores.push(valores[valores.length - 1] / valores[valores.length - 2])
         }
     }
-}
 
-//criando uma variável para receber o resultado das operações
+    res = valores[valores.length - 1]
+    console.log(valores.length - 1)
 
-res = valores [ valores.length - 1]
-
-//criando função para alertar o valor do campo visor
-
-function mostraValor (res){
     let a = document.querySelector('input[name="res"]')
 
     if (res){
         a.value = res  
     } else {
-        a.value =""
+        a.value = 0
     }    
     console.log(a.value)
 
     aux = 0
 }
-
